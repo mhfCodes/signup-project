55dcc89e24b423d8497f118f81e0555aeb765d59
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-01-29 04:02:33 +0330

    added logout section

--- src/main/java/com/hossein/signup/LogoutServlet.java
+++ src/main/java/com/hossein/signup/LogoutServlet.java
@@ -2,10 +2,12 @@ package com.hossein.signup;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
@@ -16,6 +18,19 @@ public class LogoutServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		response.setContentType("text/html");
+		request.getRequestDispatcher("links.html").include(request, response);
+		
+		Cookie cookie = new Cookie("username", "");
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+		
+		out.print("You Have Successfully Logged Out");
+		
+		out.close();
 		
 	}
 
